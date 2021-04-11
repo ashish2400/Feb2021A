14c029054546d28c511045ccfd32e5672f15a62f
Ashish <Ashish@LAPTOP-TOD4QBS9>
2021-04-11 18:20:16 +0530

    [Ashish]: first code pushed

---
+++ src/git/sample1.java
@@ -0,0 +1,10 @@
+package git;
+
+public class sample1 {
+public static void main(String[] args) {
+	
+	System.out.println("1st code push to team lead Kuldeep");
+	int b= 25;
+	System.out.println(b);
+}
+}
